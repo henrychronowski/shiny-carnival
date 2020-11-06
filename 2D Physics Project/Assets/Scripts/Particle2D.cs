f90fc9a25bcd434599723a9fe75e52d2beafd35a
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2020-11-06 01:34:53 -0500

    Adding initial PhysicsObject2D framework

--- 2D Physics Project/Assets/Scripts/Particle2D.cs
+++ 2D Physics Project/Assets/Scripts/Particle2D.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Particle2D : MonoBehaviour
+public class Particle2D : PhysicsObject2D
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
---
+++ 2D Physics Project/Assets/Scripts/PhysicsObject2D.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject2D : MonoBehaviour
+{
+	protected Vector2 mPos;
+	protected Vector2 mVel;
+	protected Vector2 mAcc;
+	protected Vector2 mAccumulatedForces;
+	protected float mFacing;
+	protected float mRotVel;
+	protected float mRotAcc;
+	protected float mDampingConstant;
+	protected float mInverseMass;
+	protected bool mShouldIgnoreForces;
+
+    void Start()
+    {
+		// Get singleton integrator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+		// Call integrator/pass data (maybe a data struct would actually be usefull)
+		// Or actually having an integrate function here that the integrator calls, conversely
+    }
+
+	void clearAccumulatedForces()
+	{
+		mAccumulatedForces = (0.0f, 0.0f);
+	}
+
+	void Integrate( double dt )
+	{
+		mPos += mVel * (float)dt;
+
+		Vector2 resultingAcc = mAcc;
+
+		if(!mShouldIgnoreForces)
+		{
+			resultingAcc += mAccumulatedForces * mInverseMass;
+		}
+
+		mVel += resultingAcc * (float)dt;
+		float damping = Mathf.Pow(mDampingConstant, (float)dt);
+		mVel *= damping;
+	}
+}
