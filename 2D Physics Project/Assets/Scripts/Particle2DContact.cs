d947516a3ccb3f4a058f1cd5459d3259376c927d
Henry Chronowski <henry.chronowski@mymail.champlain.edu>
2020-11-09 01:53:23 -0500

    Adding test scene, contact work

--- 2D Physics Project/Assets/Scripts/Particle2DContact.cs
+++ 2D Physics Project/Assets/Scripts/Particle2DContact.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Particle2DContact : MonoBehaviour
+public class Particle2DContact
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float restitutionCoefficient { get; set; }
+	private float penetration { get; set; }
 }
