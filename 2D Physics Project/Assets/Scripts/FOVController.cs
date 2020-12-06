6a37bbff907cd4cc00bbd664161a7ea37cb3c0a4
Ethan Heil <eheil20@gmail.com>
2020-12-05 20:30:16 -0500

    Adding FOVController to control the camera's FOV with the scroll wheel

---
+++ 2D Physics Project/Assets/Scripts/FOVController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVController : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+    [SerializeField]
+    private float minFOV = 30.0f;
+    [SerializeField]
+    private float maxFOV = 150.0f;
+    [SerializeField]
+    private float increment = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            mainCamera.fieldOfView += increment;
+            mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, minFOV, maxFOV);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            mainCamera.fieldOfView -= increment;
+            mainCamera.fieldOfView = Mathf.Clamp(mainCamera.fieldOfView, minFOV, maxFOV);
+        }
+    }
+}
